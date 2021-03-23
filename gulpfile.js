84000447f1bec120908b2fe0fb6e85deaba34778
harish <koninti9@gmail.com>
2021-03-24 00:23:42 +0530

    combined js files

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('css-vendor', function(){
     .pipe(minifyCSS())
     .pipe(autoprefixer('last 2 version','safari 5','ie 9'))
     .pipe(gp_concat('vendor.min.css'))
-    .pipe(gulp.dest('./public/dist/vendor/css/'))
+    .pipe(gulp.dest('./public/dist/css/'))
 })
 
 gulp.task('copy-fonts',function(){
@@ -52,3 +52,37 @@ gulp.task('copy-fonts',function(){
 
 })
 
+gulp.task('js-vendor', function(){
+    return gulp.src(
+        [
+
+            './public/vendor/jquery/jquery-3.2.1.min.js',
+            './public/vendor/wow/wow.min.js',
+            './public/vendor/animsition/dist/js/animsition.min.js',
+            './public/vendor/bootstrap/js/popper.min.js',
+            './public/vendor/bootstrap/js/bootstrap.min.js',
+            './public/vendor/revolution/js/jquery.themepunch.tools.min.js',
+            './public/vendor/revolution/js/jquery.themepunch.revolution.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.video.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.carousel.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.slideanims.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.actions.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.layeranimation.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.kenburn.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.navigation.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.migration.min.js',
+            './public/vendor/revolution/js/extensions/revolution.extension.parallax.min.js',
+            './public/js/slide-custom.js',
+            './public/vendor/select2/select2.min.js',
+            './public/vendor/daterangepicker-bootstrap/moment.min.js',
+            './public/vendor/daterangepicker-bootstrap/daterangepicker.js',
+            './public/js/main.js'
+        ]
+    )
+    .pipe(gp_concat('vendor.min.js'))
+    .pipe(gulp.dest('./public/dist/js/'))
+    .pipe(gp_rename('vendor.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('./public/dist/js/'))
+
+});
